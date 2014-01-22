62db53126d0fab94f8fe84dd42d2e961cf2026b9
wangyou <1078360711@qq.com>
2014-01-22 16:42:40 +0800

    Add Hybrid Support

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -268,6 +268,7 @@ static const struct fs_path_config android_files[] = {
     { 00550, AID_ROOT,      AID_SHELL,     0, "system/etc/init.testmenu" },
     { 00550, AID_DHCP,      AID_SHELL,     0, "system/etc/dhcpcd/dhcpcd-run-hooks" },
     { 00440, AID_BLUETOOTH, AID_BLUETOOTH, 0, "system/etc/dbus.conf" },
+    { 00644, AID_SYSTEM,    AID_SYSTEM,    0, "system/etc/paranoid/properties.conf" },
     { 00444, AID_RADIO,     AID_AUDIO,     0, "system/etc/AudioPara4.csv" },
     { 00555, AID_ROOT,      AID_ROOT,      0, "system/etc/ppp/*" },
     { 00555, AID_ROOT,      AID_ROOT,      0, "system/etc/rc.*" },
